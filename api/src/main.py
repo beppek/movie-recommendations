3b9fa97b67e8e48375c62c487f2edab43a6791ca
beppek <beppe@live.se>
2018-01-15 21:59:25 +0100

    add movies route

--- api/src/main.py
+++ api/src/main.py
@@ -4,6 +4,7 @@ from flask_cors import CORS
 from routes.entry import entry
 from routes.ping import ping
 from routes.users import users
+from routes.movies import movies
 
 app = Flask(__name__)
 
@@ -12,6 +13,7 @@ CORS(app)
 app.register_blueprint(entry)
 app.register_blueprint(ping)
 app.register_blueprint(users)
+app.register_blueprint(movies)
 
 if __name__ == '__main__':
   app.run(host='127.0.0.1', port=8080, debug=True)
