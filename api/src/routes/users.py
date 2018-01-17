cabe25198f555734cfc10c8cc7fd857f580e8175
beppek <beppe@live.se>
2018-01-17 21:03:38 +0100

    add filtering to users route

--- api/src/routes/users.py
+++ api/src/routes/users.py
@@ -1,8 +1,8 @@
 """USERS"""
 
 from flask import Blueprint, jsonify
-from utils.file_reader import read_ratings, read_users, read_movies
-from utils.similarity_calculator import euclidean_distance, pearson_correlation, top_matches, get_recommendations
+from utils.file_reader import read_ratings, read_users, read_movies, read_item_based_data, calc_similar_items
+from utils.similarity_calculator import euclidean_distance, pearson_correlation, top_matches, get_recommendations, transform_prefs, get_recommended_items
 
 users = Blueprint('users', __name__)
 
@@ -28,14 +28,24 @@ def user_matches(_id):
     data = top_matches(read_ratings(), _id, 100, pearson_correlation)
     return jsonify(data)
 
-@users.route('/users/<_id>/recommendations/euclidean')
-def user_euclidean_recommendations(_id):
-    ratings = read_ratings()
-    data = get_recommendations(ratings, _id, euclidean_distance)
+@users.route('/users/<_id>/recommendations/<filtering>/euclidean')
+def user_euclidean_recommendations(_id, filtering):
+    if filtering == 'user':
+        ratings = read_ratings()
+        data = get_recommendations(ratings, _id, euclidean_distance)
+    else:
+        ratings = read_ratings()
+        item_based = read_item_based_data('euclidean', euclidean_distance)
+        data = get_recommended_items(ratings, item_based, _id)
     return jsonify(data)
 
-@users.route('/users/<_id>/recommendations/pearson')
-def user_pearson_recommendations(_id):
-    ratings = read_ratings()
-    data = get_recommendations(ratings, _id, pearson_correlation)
+@users.route('/users/<_id>/recommendations/<filtering>/pearson')
+def user_pearson_recommendations(_id, filtering):
+    if filtering == 'user':
+        ratings = read_ratings()
+        data = get_recommendations(ratings, _id, pearson_correlation)
+    else:
+        ratings = read_ratings()
+        item_based = read_item_based_data('pearson', pearson_correlation)
+        data = get_recommended_items(ratings, item_based, _id)
     return jsonify(data)
